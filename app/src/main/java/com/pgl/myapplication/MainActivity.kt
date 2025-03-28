39783c2e2130325ebb80e80d9163d899a63de4df
PEDRO <patatero1962@gmail.com>
2025-03-28 20:37:06 +0100

    Commit cambio el nombre

--- app/src/main/java/com/pgl/myapplication/MainActivity.kt
+++ app/src/main/java/com/pgl/myapplication/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Solo Modifico el nombre pglAndroid",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
